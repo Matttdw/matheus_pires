8990691784dc1b15e5b1f5c1f65ea31f564625f7
Matttdw <mathctbapires@outlook.com>
2023-11-16 20:23:59 -0300

    Colocando o botao para mudar d ecor

---
+++ scriptWhile.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    // Seleciona o elemento do menu
+    var menu = document.getElementById('menu');
+
+    // Alterna a classe para mudar a cor
+    menu.classList.toggle('changed-color');
+}
